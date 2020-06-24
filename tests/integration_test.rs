444914e3458ab6e1311e621eca1d21d60d0e5b4f
Nuno Pinheiro <nuno.pinheiro@smallgiantgames.com>
2020-06-24 19:22:50 +0300

    Fix warns from tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,10 +1,9 @@
-use std::sync::Arc;
 use std::thread;
 use web_server::http::http_server::HttpServer;
 #[test]
 fn test() {
     let mut server = HttpServer::new("127.0.0.1", 7878);
 
-    server.get("/path", |x| println!("Called"));
+    server.get("/path", |_| println!("Called"));
     thread::spawn(|| server.listen());
 }
